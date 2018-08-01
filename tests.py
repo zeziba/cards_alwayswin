13b2037d31d585ebdd13265071f241a4d40f743d
Charles Engen <cengen@student.cccs.edu>
2018-07-31 19:59:51 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,1 +1,36 @@
+import unittest
 
+import pd_main
+
+
+class TestMethods(unittest.TestCase):
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_pair(self):
+        self.assertEqual(pd_main.dice_rules(), ['Pair'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+        
+    def test_bust(self):
+        self.assertEqual(pd_main.dice_rules(), ['Bust'])
+    
+    def test_die(self):
+        dice = pd_main.Dice()
+
+
+if __name__ == '__main__':
+unittest.main()
